73fe43242c5e37a410755eef4fac6183ec9764b4
Alex <arusskikhqa@gmail.com>
2022-05-11 09:14:40 -0400

    init:start JS ES6 Project

---
+++ 1_let_const.js
@@ -0,0 +1,35 @@
+// var num = 43;
+// num = "str";
+// console.log(num);
+
+// if (true) {
+//   let a = 42;
+// }
+// console.log(a);
+
+//Hoisting
+// b = 20;
+// console.log(b);
+// let b = 10;
+
+// function hoisted() {
+//   age = 26;
+// }
+
+// let age;
+// hoisted();
+// console.log(age);
+
+//Const
+// const COLOR = "#ffeebb";
+// COLOR = "#000";
+// console.log(COLOR);
+
+// const array = [1, 2, 3, 4, 5, 6];
+// console.log(array);
+// array.push(13);
+// console.log(array);
+
+const obj = { a: 42 };
+obj.name = "Igor";
+console.log(obj);
